e78b568cd35b68ca618c44d705daf04b68bd911a
carlosomillan <carlosomillan@gmail.com>
2022-03-30 15:02:07 -0400

    Fixed Semantic

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import 'semantic-ui-css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
     <App />,
@@ -14,3 +14,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
